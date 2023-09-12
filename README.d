fe39d5da380170617060fa681e3a12a1b309a6b6
Derk Snijders <derk.snijders@carthago-ict.nl>
2023-09-12 16:22:13 +0200

    include readme

--- README.d
+++
@@ -1,3 +0,0 @@
-    Replace the placeholders (client_id, redirect_uri, scope) with your actual OAuth details.
-    Serve this HTML file via an HTTP server (e.g., you can use npx serve to serve the current directory).
-    Navigate to http://localhost:3000 (or whatever port you set).
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BarebonesOAuthSample
+
+1. Configure `clientId` and `redirectUri` in both `main.js` and `callback.js`
+2. `npx serve` (defaults to port 3000)
